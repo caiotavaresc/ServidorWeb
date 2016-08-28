c305e3f619fd2e1f507270468f1bda6da12f6d9d
SirSalerno <bruno@mba.local>
2016-08-27 21:03:32 -0300

    Criação do Socket na porta 6789

--- src/servidorweb/ServidorWeb.java
+++ src/servidorweb/ServidorWeb.java
@@ -1,21 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package servidorweb;
 
-/**
- *
- * @author bruno
- */
+import java.io.* ;
+import java.net.* ;
+import java.util.* ;
+
 public class ServidorWeb {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) throws Exception{
+        //Definicao de porta do listener
+        int port = 6789; 
+        //Criacao do socket para a porta escolhida
+        ServerSocket socket = new ServerSocket(port);
+        //while(true) para estar sempre aceitando requisicoes
+        while(true){
+            //Aceita conexao
+            Socket connectionSocket = socket.accept();
+            //Le conexao e imprime o que foi recebido
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            System.out.println(inFromClient.readLine());
+        }
+   }
+    
+    
+}
+
+final class HttpRequest implements Runnable
+{
+
+   
+    public void run() {
+      }
+
 }
